7b80850b95cc6afbb6bee0034368448a4512f565
redbean88 <doctortor88@gmail.com>
2023-09-01 15:06:25 +0900

    네이버써트 추가

---
+++ src/main/java/com/barocert/springboot/autoconfigure/NavercertAutoConfiguration.java
@@ -0,0 +1,45 @@
+package com.barocert.springboot.autoconfigure;
+
+import com.barocert.navercert.NavercertService;
+import com.barocert.navercert.NavercertServiceImp;
+import com.barocert.springboot.autoconfigure.properties.KakaocertServiceProperties;
+import com.barocert.springboot.autoconfigure.properties.NavercertServiceProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+@ConditionalOnClass(NavercertService.class)
+@EnableConfigurationProperties({NavercertServiceProperties.class})
+public class NavercertAutoConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(NavercertAutoConfiguration.class);
+    
+    @Autowired
+    private NavercertServiceProperties navercertServiceProperties;
+    
+    @Lazy
+    @Bean(name = "NavercertService")
+    @ConditionalOnMissingBean
+    public NavercertService NavercertServiceConfig() {
+
+        NavercertServiceImp navercertServiceImp = new NavercertServiceImp();
+
+        navercertServiceImp.setLinkID(navercertServiceProperties.getLinkID());
+        navercertServiceImp.setSecretKey(navercertServiceProperties.getSecretKey());
+        navercertServiceImp.setUseStaticIP(navercertServiceProperties.isUseStaticIP());
+        navercertServiceImp.setIPRestrictOnOff(navercertServiceProperties.isIsIPRestrictOnOff());
+        navercertServiceImp.setServiceURL(navercertServiceProperties.getServiceURL());
+        navercertServiceImp.setAuthURL(navercertServiceProperties.getAuthURL());
+
+        logger.debug("NavercertService Initialized");
+
+        return navercertServiceImp;
+    }
+}
---
+++ src/main/java/com/barocert/springboot/autoconfigure/properties/NavercertServiceProperties.java
@@ -0,0 +1,71 @@
+package com.barocert.springboot.autoconfigure.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "navercert")
+public class NavercertServiceProperties {
+
+    private String LinkID = null;
+    private String SecretKey = null;
+    private boolean IsIPRestrictOnOff = true;
+    private boolean UseStaticIP = false;
+    private boolean UseLocalTimeYN = true;
+    private String serviceURL = null;
+    private String authURL = null;
+
+    public String getLinkID() {
+        return LinkID;
+    }
+
+    public void setLinkID(String linkID) {
+        LinkID = linkID;
+    }
+
+    public String getSecretKey() {
+        return SecretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        SecretKey = secretKey;
+    }
+
+    public boolean isIsIPRestrictOnOff() {
+        return IsIPRestrictOnOff;
+    }
+
+    public void setIsIPRestrictOnOff(boolean isIPRestrictOnOff) {
+        IsIPRestrictOnOff = isIPRestrictOnOff;
+    }
+
+    public boolean isUseStaticIP() {
+        return UseStaticIP;
+    }
+
+    public void setUseStaticIP(boolean useStaticIP) {
+        UseStaticIP = useStaticIP;
+    }
+
+    public boolean isUseLocalTimeYN() {
+        return UseLocalTimeYN;
+    }
+
+    public void setUseLocalTimeYN(boolean useLocalTimeYN) {
+        UseLocalTimeYN = useLocalTimeYN;
+    }
+
+    public String getServiceURL() {
+        return this.serviceURL;
+    }
+
+    public void setServiceURL(String serviceURL) {
+        this.serviceURL = serviceURL;
+    }
+
+    public String getAuthURL() {
+        return this.authURL;
+    }
+
+    public void setAuthURL(String authURL) {
+        this.authURL = authURL;
+    }
+}
